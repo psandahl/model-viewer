458cdcb2874ff460204070f605ec68fd81fbad68
Patrik Sandahl <patrik.sandahl@ericsson.com>
2017-03-15 13:42:34 +0100

    New camera zoom.

--- src/Camera.hs
+++ src/Camera.hs
@@ -9,39 +9,53 @@ import           Graphics.LWGL (GLfloat)
 import           Linear        (M44, V3 (..), lookAt)
 
 data Camera = Camera
-    { view :: !(M44 GLfloat)
-    , pos  :: !(V3 GLfloat)
+    { view     :: !(M44 GLfloat)
+    , angle    :: !GLfloat
+    , distance :: !GLfloat
+    , height   :: !GLfloat
     } deriving Show
 
 initCamera :: Camera
 initCamera =
-    let pos' = V3 0 0 5
+    let angle' = 0
+        distance' = 5
+        height' = 5
     in
-        Camera { view = makeView pos'
-               , pos = pos'
+        Camera { view = makeView $ makeCameraPosition angle' distance' height'
+               , angle = angle'
+               , distance = distance'
+               , height = height'
                }
 
 moveForward :: Camera -> Camera
 moveForward camera =
-    let newPos = pos camera - unit
-    in if newPos > closest
-           then camera { view = makeView newPos
-                       , pos = newPos
+    let newDistance = distance camera - 0.5
+    in if newDistance > 1.0
+           then camera { distance = newDistance
+                       , view = makeView $ makeCameraPosition (angle camera)
+                                                              newDistance
+                                                              (height camera)
                        }
            else camera
 
 moveBackward :: Camera -> Camera
 moveBackward camera =
-    let newPos = pos camera + unit
-    in camera { view = makeView newPos
-              , pos = newPos
+    let newDistance = distance camera + 0.5
+    in camera { distance = newDistance
+              , view = makeView $ makeCameraPosition (angle camera)
+                                                     newDistance
+                                                     (height camera)
               }
-
-closest :: V3 GLfloat
-closest = V3 0 0 1
-
-unit :: V3 GLfloat
-unit = V3 0 0 0.05
 
 makeView :: V3 GLfloat -> M44 GLfloat
 makeView pos' = lookAt pos' (V3 0 0 0) (V3 0 1 0)
+
+makeCameraPosition :: GLfloat -> GLfloat -> GLfloat -> V3 GLfloat
+makeCameraPosition angle' distance' height' =
+    let (V3 x _ z) = unitAtAngle angle'
+    in (V3 (distance' * x) height' (distance' * z))
+
+-- | Unit vector, with no height/y value, but with a given angle. At angle zero
+-- the vector points at x = 0, z = 1.
+unitAtAngle :: GLfloat -> V3 GLfloat
+unitAtAngle angle' = V3 (sin angle') 0 (cos angle')
